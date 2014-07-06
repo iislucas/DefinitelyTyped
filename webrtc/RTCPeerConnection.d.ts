7f2edee2ea0750a18d1dc74e3da7e361a97d5047
Lucas Dixon <ldixon@google.com>
2014-07-06 14:56:15 -0400

    fixes for getStats

--- webrtc/RTCPeerConnection.d.ts
+++ webrtc/RTCPeerConnection.d.ts
@@ -212,15 +212,27 @@ declare enum RTCSignalingState {
   'closed'
 }
 
-// This is based on the current implementation of WebRtc in Chrome; the spec is
-// a little unclear on this.
+// This is based on the current implementation of WebRtc:
 // http://dev.w3.org/2011/webrtc/editor/webrtc.html#idl-def-RTCStatsReport
+declare enum RTCStatsType {
+  "inbound-rtp",
+  "outbound-rtp"
+}
+
+interface RTCStats {
+  timestamp: Object;  // TODO: find/define the type for DOMHiResTimeStamp
+  type: string;  // RTCStatsType;
+  id: string;
+  stat?: (s:string) => string;
+}
+
 interface RTCStatsReport {
-  stat(id: string): string;
+  result: () => RTCStats[]
+  namedItem: () => void;
 }
 
 interface RTCStatsCallback {
-  (report: RTCStatsReport): void;
+  (report: RTCStatsReport) : void
 }
 
 interface RTCPeerConnection {
@@ -235,13 +247,15 @@ interface RTCPeerConnection {
                       failureCallback?: RTCPeerConnectionErrorCallback): void;
   localDescription: RTCSessionDescription;
   setRemoteDescription(description: RTCSessionDescription,
-                        successCallback?: RTCVoidCallback,
-                        failureCallback?: RTCPeerConnectionErrorCallback): void;
+                       successCallback?: RTCVoidCallback,
+                       failureCallback?: RTCPeerConnectionErrorCallback): void;
   remoteDescription: RTCSessionDescription;
   signalingState: string; // RTCSignalingState; see TODO(1)
   updateIce(configuration?: RTCConfiguration,
             constraints?: RTCMediaConstraints): void;
-  addIceCandidate(candidate: RTCIceCandidate): void;
+  addIceCandidate(candidate: RTCIceCandidate,
+                  successCallback?: RTCVoidCallback,
+                  failureCallback?: RTCPeerConnectionErrorCallback): void;
   iceGatheringState: string;  // RTCIceGatheringState; see TODO(1)
   iceConnectionState: string;  // RTCIceConnectionState; see TODO(1)
   getLocalStreams(): MediaStream[];
